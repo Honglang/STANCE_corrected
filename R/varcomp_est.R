749b779e507dd59784afb4a6aef9b60d52a4e343
HaroldSu <haroldsu.us@gmail.com>
2024-07-03 17:41:55 -0400

    fix estimation in varcomp_est

--- R/varcomp_est.R
+++ R/varcomp_est.R
@@ -6,21 +6,19 @@
 #' claiming all the genes of interest to estimate.
 #' If NULL, estimation of variance components will perform across all the genes.
 #' @return return STANCE object.
-#' 
-#' @import MM4LMM
-#' 
+#'
+#' @import gaston
+#'
 #' @export
 
 varcomp_est <- function(object, Genes_to_est = NULL){
   n <- dim(object@gene_expression)[2]
   K <- dim(object@cell_type_compositions)[2]
   numGenes <- dim(object@gene_expression)[1]
-  
-  In <- diag(rep(1, n))
-  jn <- matrix(rep(1,n), nrow = n)
-  X <- cbind(jn, object@covariates)
+
+  X <- cbind(matrix(1,nrow = n), object@covariates)
   counts <- object@gene_expression
-  
+
   if(!is.null(Genes_to_est)){
     genes_to_est.list <- intersect(row.names(counts), Genes_to_est)
     if(identical(genes_to_est.list, character(0))){
@@ -31,22 +29,17 @@ varcomp_est <- function(object, Genes_to_est = NULL){
   } else {
     counts.use <- counts
   }
-  
+
   Est_result <- apply(counts.use, MARGIN = 1, function(y){
     ## Full model
-    Var.list <- object@Covariance_matrices
-    Var.list[[K + 1]] <- In
-    Z.list <- object@Random_effect_design_matrices
-    Z.list[[K + 1]] <- In
-    model.full <- MM4LMM::MMEst(Y = y, Cofactor = object@covariates,
-                                VarList = Var.list,
-                                ZList = Z.list,
-                                Verbose = FALSE)
-    output <- model.full$NullModel$Sigma2
+    model.full <- gaston::lmm.aireml(Y = y, X = X,
+                                     K = object@Sigma_k_matrices,
+                                     verbose = FALSE)
+    output <- c(model.full$tau, model.full$sigma2)
     names(output) <- c(object@cell_types, "Error_residuals")
     return(output)
   })
-  
+
   object@VarComp_estimates <- as.data.frame(t(Est_result))
   return(object)
 }
