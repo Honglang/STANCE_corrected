ca07663da695bbaa1cc5a84588c8425c36d5f5a5
Haohao Su <haroldsu.us@gmail.com>
2025-02-17 21:29:13 -0500

    fixed the typo in createSTANCEobject

--- R/createSTANCEobject.R
+++ R/createSTANCEobject.R
@@ -1,7 +1,7 @@
 #' @title Create the STANCE object.
 #' @param counts original gene expression matrix of dimension n x G,
-#' with genes in rows and spots in columns. 
-#' @param pos the n x 2 dimensional location coordinates matrix. 
+#' with genes in rows and spots in columns.
+#' @param pos the n x 2 dimensional location coordinates matrix.
 #' The rownames of pos matrix should match the colnames of counts matrix.
 #' @param prop the cell type proportion of each spot (n x K matrix)
 #' Rows represent spots and columns represent cell types.
@@ -22,7 +22,7 @@ creatSTANCEobject <- function(counts, pos, prop, covariates = NULL){
       stop('\'counts\' could not be converted to matrix. Please check that \'counts\' is coercible to matrix, such as a matrix, dgCmatrix, or data.frame.')
     })
   }
-  
+
   if (!is.matrix(prop)) {
     tryCatch({
       prop <- as.matrix(prop)
@@ -30,7 +30,7 @@ creatSTANCEobject <- function(counts, pos, prop, covariates = NULL){
       stop('\'prop\' could not be converted to matrix. Please check that \'prop\' is coercible to matrix, such as a matrix, dgCmatrix, or data.frame.')
     })
   }
-  
+
   if (!is.matrix(pos)) {
     tryCatch({
       pos <- as.matrix(pos)
@@ -38,27 +38,27 @@ creatSTANCEobject <- function(counts, pos, prop, covariates = NULL){
       stop('\'pos\' could not be converted to matrix. Please check that \'pos\' is coercible to matrix, such as a matrix, dgCmatrix, or data.frame.')
     })
   }
-  
+
   genes <- rownames(counts)
   if(is.null(genes)) {
     stop('\'rownames(counts)\' is null!')
   }
-  
+
   barcodes <- colnames(counts)
   if(is.null(barcodes)) {
     stop('\'colnames(counts)\' is null!')
   }
-  
+
   spots <- intersect(intersect(barcodes, rownames(pos)), rownames(prop))
   pos.use <- pos[match(spots, rownames(pos)),]
   prop.use <- prop[match(spots, rownames(prop)),]
-  counts.use <- counts[,match(spots, colnames(counts))]
+  counts.use <- counts[match(spots, colnames(counts)),]
   if(!is.null(covariates)){
     covariates.use <- covariates[,match(spots, rownames(covariates))]
   }else{
     covariates.use <- NULL
   }
-  
+
   object <- methods::new(
     Class = "STANCE",
     gene_expression = counts.use,
